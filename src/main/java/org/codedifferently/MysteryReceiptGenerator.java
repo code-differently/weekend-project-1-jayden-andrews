2a4535dd871ec4b2d61e3844cded6c6608991937
Jayden Andrews <jaydenrandrews@gmail.com>
2026-02-07 11:11:59 -0500

    Debugged calculator methods and finished most of the reciept method

--- src/main/java/org/codedifferently/MysteryReceiptGenerator.java
+++ src/main/java/org/codedifferently/MysteryReceiptGenerator.java
@@ -30,11 +30,17 @@ class MysteryReceiptGenerator {
     }
 
     public static double generateTax(Random random) {
-        return (Math.round(random.nextDouble(0.01, 0.07)));
+        return ((Math.floor(random.nextDouble(2, 7))) / 100);
     }
 
     public static double calculateSubtotal(double price1, double price2, double price3) {
         return price1 + price2 + price3;
+    }
+
+    public static double calculateFinalTotal(double subtotal, double tax, double discount) {
+        double finalTotal = Math.ceil((subtotal + (subtotal * tax)) * 100) / 100;
+        finalTotal = Math.ceil((finalTotal - (finalTotal * discount)) * 100) / 100;
+        return finalTotal;
     }
 
     public static void printReceipt(String name, double budget, String coupon) {
@@ -56,7 +62,7 @@ class MysteryReceiptGenerator {
         double price3 = MysteryReceiptGenerator.generatePrice(random);
         double tax = MysteryReceiptGenerator.generateTax(random);
         double subtotal = MysteryReceiptGenerator.calculateSubtotal(price1, price2, price3);
-        double discount = MysteryReceiptGenerator.validateCoupon(coupon);
+
 
         // Prints the receipt to the console
         System.out.println("\n" +MysteryReceiptGenerator.name);
@@ -68,8 +74,12 @@ class MysteryReceiptGenerator {
         System.out.println("Item 3: $" + price3);
         System.out.println("Subtotal: $" + subtotal);
         System.out.println("\n-Price Adjustments-");
-        System.out.println("Sales tax: " + (tax * 100) + "%");
-
+        System.out.println("Sales tax: " + ((int)(tax * 100)) + "%");
+        double discount = MysteryReceiptGenerator.validateCoupon(coupon);
+        if (discount != 0.0) {
+            System.out.println("Discount: " + (int)(discount * 100) + "%");
+        }
+        System.out.println("Final Total: $" + MysteryReceiptGenerator.calculateFinalTotal(subtotal, tax, discount));
 
     }
 }
