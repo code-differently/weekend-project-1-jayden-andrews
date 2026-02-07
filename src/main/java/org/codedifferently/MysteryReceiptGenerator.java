a11f3d6f980517d7300db7f5640574562fcfde62
Jayden Andrews <jaydenrandrews@gmail.com>
2026-02-07 12:21:08 -0500

    Fixed decimal formatting and some calculations.

--- src/main/java/org/codedifferently/MysteryReceiptGenerator.java
+++ src/main/java/org/codedifferently/MysteryReceiptGenerator.java
@@ -5,14 +5,15 @@ class MysteryReceiptGenerator {
     static String name = "Jayden's Emporium";
 
     public static double validateCoupon(String coupon) {
+        System.out.println("\nChecking for coupon code...");
         if ((coupon).equalsIgnoreCase("friend")) {
-            System.out.println("-Coupon code " + "\"" + coupon + "\" accepted-");
+            System.out.println("Coupon code " + "\"" + coupon + "\" accepted!");
             return 0.15;
         } else if ((coupon).equalsIgnoreCase("employee")) {
-            System.out.println("-Coupon code " + "\"" + coupon + "\" accepted-");
+            System.out.println("Coupon code " + "\"" + coupon + "\" accepted!");
             return 0.30;
         } else {
-            System.out.println("-Coupon code not found.-");
+            System.out.println("Coupon code " + "\"" + coupon + "\" not found!");
             return 0.0;
         }
     }
@@ -64,22 +65,31 @@ class MysteryReceiptGenerator {
         double subtotal = MysteryReceiptGenerator.calculateSubtotal(price1, price2, price3);
 
 
+
         // Prints the receipt to the console
         System.out.println("\n" +MysteryReceiptGenerator.name);
         System.out.println("----------------------------------------------------------------");
         System.out.println("ID Code: " + visitorID);
         System.out.println("Receipt Code: " + receiptCode);
-        System.out.println("Item 1: $" + price1);
-        System.out.println("Item 2: $" + price2);
-        System.out.println("Item 3: $" + price3);
-        System.out.println("Subtotal: $" + subtotal);
-        System.out.println("\n-Price Adjustments-");
+        System.out.printf("Item 1: $%.2f%n", price1);
+        System.out.printf("Item 2: $%.2f%n", price2);
+        System.out.printf("Item 3: $%.2f%n", price3);
+        System.out.printf("Subtotal: $%.2f%n", subtotal);
+        System.out.println("\n---Price Adjustments---");
         System.out.println("Sales tax: " + ((int)(tax * 100)) + "%");
         double discount = MysteryReceiptGenerator.validateCoupon(coupon);
+        double finalTotal = MysteryReceiptGenerator.calculateFinalTotal(subtotal, tax, discount);
         if (discount != 0.0) {
             System.out.println("Discount: " + (int)(discount * 100) + "%");
         }
-        System.out.println("Final Total: $" + MysteryReceiptGenerator.calculateFinalTotal(subtotal, tax, discount));
+        System.out.printf("\nFinal Total: $%.2f%n", finalTotal);
+
+        if (finalTotal > budget) {
+            System.out.printf("\nTransaction failed!\nInsufficient Funds. User is short by $%.2f dollars...\nCancelling transaction.", Math.abs(budget - finalTotal));
+        } else {
+
+            System.out.println("\nTransaction succeeded!\nThank you for shopping at Jayden's Emporium!");
+        }
 
     }
 }
